7c5db7c17f0f716bbdb2e9f9e14a77c0cff303fa
Lmp-off <Jelesha@protonmail.ch>
2021-01-23 13:08:18 +0300

    cosmetic changes

--- app/src/main/java/com/example/illthinkaboutit/MainActivity.java
+++ app/src/main/java/com/example/illthinkaboutit/MainActivity.java
@@ -10,26 +10,30 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    FloatingActionButton floatButton;
     ListView listView;
     public static final String CircleAction="com.example.illthinkaboutit.Show Circle";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button =findViewById(R.id.button);
-        button.setOnClickListener(view -> {
-            Toast.makeText(this,"S",Toast.LENGTH_SHORT).show();
-        });
         ArrayList<String> arrayList = new ArrayList<>();
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
+        floatButton =findViewById(R.id.floatingActionButton2);
+        floatButton.setOnClickListener(view -> {
+            arrayList.add(String.valueOf(arrayList.size()+1));
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
+            listView.setAdapter(arrayAdapter);
+            listView.scrollListBy(12);
+            listView.deferNotifyDataSetChanged();
+        });
         listView = findViewById(R.id.View);
-        listView.setAdapter(arrayAdapter);
         button = findViewById(R.id.previewCircle);
 
 
